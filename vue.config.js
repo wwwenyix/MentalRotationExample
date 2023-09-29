28ad5d0e08a6a8b5a005e6caf597284dae9a157d
wwwenyix <73879809+wwwenyix@users.noreply.github.com>
2023-09-29 15:26:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   publicPath:
     process.env.NODE_ENV === 'production' && process.env.REPO_NAME
-      ? 'https://github.com/wwwenyix/MentalRotationExample/tree/gh-pages/' +
+      ? 'https://raw.githubusercontent.com/wwwenyix/MentalRotationExample/gh-pages/' +
         __dirname.substring(process.env.GITHUB_WORKSPACE.length)
-      : 'https://github.com/wwwenyix/MentalRotationExample/tree/gh-pages/'
+      : 'https://raw.githubusercontent.com/wwwenyix/MentalRotationExample/gh-pages/'
 };
